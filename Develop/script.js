31e242defc259ac7668103753508af4aa1ca7923
brownradical <79535503+brownradical@users.noreply.github.com>
2021-04-11 17:58:09 -0500

    complete character lists of criteria arrays

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,41 @@
-// Assignment code here
+// ARRAYS FOR CRITERIA SELECTION
+// SPECIAL CHARTACTERS ARRAY
+  var specialCharacters = [' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', "\", "]", "^", "_", '`', "{", '|', "}", '~'];
+
+// LOWERCASE ARRAY
+  var lowercaseCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+// UPPERCASE ARRAY
+  var uppercaseCharcters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+// NUMBER ARRAY
+  var numberCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+// USER CRITERIA SELECTION PROMPTS
+// PASSWORD LENGTH PROMPT
+
+// PASSWORD SPECIAL CHARACTER PROMPT
+
+// PASSWORD LOWERCASE PROMPT
+
+// PASSWORD UPPERCASE PROMPT
+
+// PASS NUMBER PROMPT
+
+
+// USER CRITERIA SELECTION STORING
+
+
+// PASSWORD GENERATION WITH CRITERIA
+
+
+// SHOW PASSWORD
+
+
+
+
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -14,7 +51,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-function writePassword () {
-  prompt ("Password length?")
-}
